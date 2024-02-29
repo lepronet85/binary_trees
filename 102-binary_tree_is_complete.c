3c28e7fc7d4ea219ac13dcc7a342273670b0ed0a
lepronet85 <lepronet85@gmail.com>
2024-02-29 13:41:00 +0100

    Add 102-binary_tree_is_complete.c

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete
+ * @tree: A pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is complete, otherwise 0
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	int is_last_level = 0;
+    binary_tree_t *queue[1024];
+    int front = 0, rear = 0;
+
+    if (tree == NULL)
+        return (0);
+
+    queue[rear++] = (binary_tree_t *)tree;
+
+    while (front < rear)
+    {
+        const binary_tree_t *current = queue[front++];
+
+        if (current == NULL)
+            is_last_level = 1;
+        else
+        {
+            if (is_last_level)
+                return (0);
+
+            queue[rear++] = current->left;
+            queue[rear++] = current->right;
+        }
+    }
+
+    return (1);
+}
+
